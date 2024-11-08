8972bbea5f62eab012cef656da67e9469a385fcb
Vitaliy <vitaliy.yanitskyy@gmail.com>
2024-11-08 10:30:08 +0200

    Add Models

---
+++ Fourier/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Fourier.Models;
+
+public class Task
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Status { get; set; }
+    public int Input { get; set; }
+    public DateTime? StartedAt { get; set; }
+    public DateTime? FinishedAt { get; set; }
+    public int Progress { get; set; }
+    public string Result { get; set; }
+    public bool IsCancelled { get; set; }
+}
