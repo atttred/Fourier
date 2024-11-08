5ff4e28c8ea947e0241665c4d08817e272d294c5
Vitaliy <vitaliy.yanitskyy@gmail.com>
2024-11-08 16:22:31 +0200

    Add cancellationtoken

---
+++ Fourier/Services/CancellationTokenService.cs
@@ -0,0 +1,39 @@
+﻿namespace Fourier.Services;
+using Fourier.Repositories;
+using Fourier.Models;
+using System;
+using System.Threading.Tasks;
+
+public interface ICancellationTokerService
+{
+    Task<CancellationToken> CreateCancellationTokenAsync(Guid problemId);
+    Task CancelProblemAsync(Guid problemId);
+}
+
+public class CancellationTokenService : ICancellationTokerService
+{
+    private readonly ICancellationTokenRepository _cancellationTokenRepository;
+    private readonly IProblemService _problemService;
+
+    public CancellationTokenService(ICancellationTokenRepository cancellationTokenRepository, IProblemService problemService)
+    {
+        _cancellationTokenRepository = cancellationTokenRepository;
+        _problemService = problemService;
+    }
+
+    public async Task<CancellationToken> CreateCancellationTokenAsync(Guid problemId)
+    {
+        var cancellationToken = new CancellationToken { TaskId = problemId };
+        return await _cancellationTokenRepository.AddAsync(cancellationToken);
+    }
+
+    public async Task CancelProblemAsync(Guid problemId)
+    {
+        var problem = await _problemService.GetTaskByIdAsync(problemId);
+        if (problem != null)
+        {
+            problem.IsCancelled = true;
+            await _problemService.UpdateTaskAsync(problem);
+        }
+    }
+}
